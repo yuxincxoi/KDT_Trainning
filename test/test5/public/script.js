e62f8df1839503ac97783772e3e0a0950eed2a9c
yuxincxoi <yjyj0774@gmail.com>
2024-05-30 17:42:49 +0900

    컴포넌트화를 위한 정적 할당

--- test/test5/public/script.js
+++ test/test5/public/script.js
@@ -3,11 +3,11 @@ const createBtn = document.getElementById("createBtn");
 const scheduleContainer = document.getElementById("scheduleContainer");
 const timeBox = document.getElementById("timeBox");
 const inputBox = document.getElementById("inputBox");
-const title = document.createElement("input");
-const time = document.createElement("input");
-const place = document.createElement("input");
-const memo = document.createElement("input");
-const saveBtn = document.createElement("button");
+const title = document.getElementById("title");
+const time = document.getElementById("time");
+const place = document.getElementById("place");
+const memo = document.getElementById("memo");
+const saveBtn = document.getElementById("saveBtn");
 
 // 24시간 표시
 for (i = 0; i < 24; i++) {
@@ -15,25 +15,6 @@ for (i = 0; i < 24; i++) {
   timeBox.append(times);
   times.innerText = i + 1;
 }
-
-// input 요소들 append하기
-inputBox.append(title, time, place, memo, saveBtn);
-
-// '저장하기' 버튼의 id
-saveBtn.id = "saveBtn";
-
-// input 요소들의 name
-title.name = "title";
-time.name = "time";
-place.name = "place";
-memo.name = "memo";
-
-// 서버로 form 전송하기 위한 설정
-inputBox.action = "submit";
-inputBox.method = "POST";
-
-// '저장하기' 버튼 type
-saveBtn.type = "submit";
 
 // opne & close 이벤트
 createBtn.addEventListener("click", () => {
