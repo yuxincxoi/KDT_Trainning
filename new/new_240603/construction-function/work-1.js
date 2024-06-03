f64c5538eb654941aa25ac6e21d8eb091455ab65
yuxincxoi <yjyj0774@gmail.com>
2024-06-03 11:53:16 +0900

    work-1 작업 완료

--- new/new_240603/construction-function/work-1.js
+++ new/new_240603/construction-function/work-1.js
@@ -6,12 +6,21 @@ function workOne(a, b) {
   // * logic : 매개변수 두 개 받기
   // * logic : return은 객체
 
-  let localVariable = {};
+  if (typeof a === "string") {
+    if (typeof b === "string") {
+      let localVariable = {};
 
-  localVariable[a] = a;
-  localVariable[b] = b;
+      localVariable[a] = a;
+      localVariable[b] = b;
 
-  return localVariable;
+      return localVariable;
+    } else {
+      console.error("b는 문자열이 아님.");
+    }
+  } else {
+    console.error("a는 문자열이 아님.");
+  }
+  // * condition : 매개변수는 모두 문자열
 }
 
 const test = workOne("나는", "최유진");
