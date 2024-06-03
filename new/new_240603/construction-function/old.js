512da44d7b8a2c0a6f8e1c0dfaa1837923751fd2
yuxincxoi <yjyj0774@gmail.com>
2024-06-03 15:33:10 +0900

    현재 생성자와 비교

--- new/new_240603/construction-function/old.js
+++ new/new_240603/construction-function/old.js
@@ -1,8 +1,16 @@
+// * old
 function old(a, b) {
   this.first = a;
   this.second = b;
 }
 
 let test = new old(1, 2);
-
 console.log(test);
+
+// * new
+class NewOld {
+  constructor(a, b) {
+    this.first = a;
+    this.second = b;
+  }
+}
