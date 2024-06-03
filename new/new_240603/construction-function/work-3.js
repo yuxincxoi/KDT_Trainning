ea4cb6e2a10e68fc467c866a4ebcc0f07cab6450
yuxincxoi <yjyj0774@gmail.com>
2024-06-03 12:02:46 +0900

    return에서 처리

--- new/new_240603/construction-function/work-3.js
+++ new/new_240603/construction-function/work-3.js
@@ -1,6 +1,14 @@
 function workThree(a, b) {
-  let local = {};
-  local[a] = "a";
-  local[b] = "b";
-  return local;
+  // let local = {};
+  // local[a] = "a";
+  // local[b] = "b";
+  // return local;
+
+  return {
+    a: a,
+    b: b,
+  };
 }
+
+const test = workThree("점심시간 ", "한시간 남음");
+console.log(test);
