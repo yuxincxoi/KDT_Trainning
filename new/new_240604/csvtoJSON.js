d349e38f115a668c2fe1a8eb31b2d96e3f0053f8
yuxincxoi <yjyj0774@gmail.com>
2024-06-04 16:35:23 +0900

    json 파일 생성 완료

--- new/new_240604/csvtoJSON.js
+++ new/new_240604/csvtoJSON.js
@@ -7,4 +7,22 @@ const rows = csvData.split("\r\n");
 
 const year = rows[0].split(",");
 
-// console.log(year);
+let data = [];
+
+for (i = 1; i < rows.length - 1; i++) {
+  const obj = {};
+  const row = rows[i].split(",");
+  for (j = 0; j < year.length; j++) {
+    if (j == 0) {
+      obj["지역"] = row[j];
+      obj[year[j]] = row[j];
+    } else if (j >= 1) {
+      obj[year[j]] = row[j];
+    }
+  }
+  data.push(obj);
+}
+
+const jsonData = JSON.stringify(data);
+
+fs.writeFileSync(path.join(__dirname, "jsonData.json"), jsonData);
