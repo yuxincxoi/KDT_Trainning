925c442608ee6461a6267157c3c5a7744afa0a65
yuxincxoi <yjyj0774@gmail.com>
2024-06-04 10:51:22 +0900

    setter 만들어서 에러 처리

--- new/new_240604/work-2.js
+++ new/new_240604/work-2.js
@@ -3,6 +3,14 @@ class WorkTow {
     this.a = a;
     this.b = b;
   }
+
+  set a(value) {
+    if (typeof value === "number") {
+      return (this._a = value);
+    } else {
+      throw new Error("a는 숫자만 입력 가능합니다.");
+    }
+  }
 }
 
 // 객체 생성
