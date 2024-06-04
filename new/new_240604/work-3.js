224fdd3a8625a3e5b5940115bbb8edcd86460842
yuxincxoi <yjyj0774@gmail.com>
2024-06-04 11:48:45 +0900

    this 헷갈릴 때 보기

--- new/new_240604/work-3.js
+++ new/new_240604/work-3.js
@@ -6,7 +6,6 @@ function workThree(a, b) {
   console.log("함수 안에 this : ", this);
   return a + b;
 }
-
 console.log(workThree(1, 2));
 
 let itsMe = {
@@ -20,6 +19,5 @@ let itsMe = {
     return this.name + "은 배고프대.";
   },
 };
-
 console.dir("객체에서 보는 this", itsMe);
 console.log("메서드로 보는 this : ", itsMe.sentence());
