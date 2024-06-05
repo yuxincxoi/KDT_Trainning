06fa0f35dedb3bd4411810035f8c00518a6df445
yuxincxoi <yjyj0774@gmail.com>
2024-06-05 11:56:39 +0900

    REST API 인증키 조립해보기

---
+++ new/new_240605/example.js
@@ -0,0 +1,16 @@
+const endPoint = `
+  http://apis.data.go.kr/6300000/eventDataService
+`;
+
+const keyName = `
+  se%2Be7mPdWCb0fBl1Vh0%2BKK6NbrNzHDbTsjqUpCMu5rT0SNgXCwIyfTz3x%2BQU6GQJ8fCy0GOMMbor6S%2FoFZzPWw%3D%3D
+`;
+
+const example = `
+  http://apis.data.go.kr/6300000/eventDataService/eventDataListJson?
+  serviceKey=인증키(URL Encode)
+  &
+  numOfRows=10
+  &
+  pageNo=1
+`;
