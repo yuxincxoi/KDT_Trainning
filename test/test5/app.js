c82a741c81e0f17c33d84e32df2dd9c47f3537c4
yuxincxoi <yjyj0774@gmail.com>
2024-06-07 23:02:26 +0900

    error response 수정

--- test/test5/app.js
+++ test/test5/app.js
@@ -14,12 +14,9 @@ const errMsg = {
   404: "404 code는 페이지를 찾을 수 없음",
 };
 
-const err500 = (err) => {
-  if (err) {
-    res.writeHead(500, { "Content-Type": mime.text });
-    res.end(errMsg[500]);
-    return;
-  }
+const err500 = (res) => {
+  res.writeHead(500, { "Content-Type": mime.text });
+  res.end(errMsg[500]);
 };
 
 const err404 = (res) => {
@@ -35,19 +32,19 @@ const server = http.createServer((req, res) => {
   if (req.method === "GET") {
     if (req.url === "/") {
       fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
-        err500(err);
+        if (err) err500(res);
         res.writeHead(200, { "Content-Type": mime.html });
         res.end(data);
       });
     } else if (req.url === "/style.css") {
       fs.readFile(path.join(__dirname, "public", "style.css"), (err, data) => {
-        err500(err);
+        if (err) err500(res);
         res.writeHead(200, { "Content-Type": mime.css });
         res.end(data);
       });
     } else if (req.url === "/script.js") {
       fs.readFile(path.join(__dirname, "public", "script.js"), (err, data) => {
-        err500(err);
+        if (err) err500(res);
         res.writeHead(200, {
           "Content-Type": mime.js,
         });
@@ -92,100 +89,88 @@ const server = http.createServer((req, res) => {
         const jsonDataString = JSON.stringify(jsonData, null, 2);
 
         // * 입력한 데이터를 JSON 형식의 파일로 생성
-        fs.writeFileSync(
-          path.join(__dirname, "data", `${title}.json`),
+        fs.writeFile(
+          path.join(__dirname, "data.json"),
           jsonDataString,
           (err) => {
-            err500(err);
+            if (err) err500(res);
             console.log("json 파일 생성");
           }
         );
 
-        // * data 폴더 list 읽기
-        let readDir = fs.readdirSync(path.join(__dirname, "data"), (err) => {
-          err500(err);
-          // let readData = JSON.parse(data);
-          console.log("Directories를 읽었다 !");
-        });
-
         // * JSON 파일 parse하여 읽기
-        readDir.forEach((element) => {
-          let readData = fs.readFileSync(
-            path.join(__dirname, "data", `${element}`),
-            (err) => {
-              console.log("json data를 읽었다 !");
-            }
-          );
-
-          let parsedReadData = JSON.parse(readData);
+        fs.readFile(path.join(__dirname, "data.json"), (err) => {
+          if (err) err500(res);
+          let data = JSON.parse(fs.readFileSync("./data.json"));
+          console.log("json 데이터를 읽었다 !");
 
           eachSchedule += `
             <div id="eachSchedule">
-              <h1>${parsedReadData.title}</h1>
-              <p>${parsedReadData.place}</p>
+              <h1>${data.title}</h1>
+              <p>${data.place}</p>
             </div>
           `;
 
           mainIdx = `
-          <!DOCTYPE html>
-          <html lang="en">
-            <head>
-              <meta charset="UTF-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-              <title>Document</title>
-              <link rel="stylesheet" href="style.css" />
-            </head>
-            <body>
-              <div id="root">
-                <div id="detail">
-                  <div id="createBtn"></div>
-                  <div id="scheduleContainer">
-                    <div id="contents">
-                      <div id="timeBox"></div>
-                      <div id="timeLine">${eachSchedule}</div>
+            <!DOCTYPE html>
+            <html lang="en">
+              <head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Document</title>
+                <link rel="stylesheet" href="style.css" />
+              </head>
+              <body>
+                <div id="root">
+                  <div id="detail">
+                    <div id="createBtn"></div>
+                    <div id="scheduleContainer">
+                      <div id="contents">
+                        <div id="timeBox"></div>
+                        <div id="timeLine">${eachSchedule}</div>
+                      </div>
+                      <form id="inputBox" action="submit" method="post">
+                        <div>
+                          <!-- <label for="title">일정</label> -->
+                          <input id="title" type="text" name="title" placeholder="일정" />
+                        </div>
+                        <div>
+                          <!-- <label for="time">시간</label> -->
+                          <input id="time" type="time" name="time" placeholder="시간" />
+                        </div>
+                        <div>
+                          <!-- <label for="place">장소</label> -->
+                          <input id="place" type="text" name="place" placeholder="장소" />
+                        </div>
+                        <div>
+                          <!-- <label for="memo">메모</label> -->
+                          <input id="memo" type="text" name="memo" placeholder="메모" />
+                        </div>
+                        <div>
+                          <button id="saveBtn" type="submit">Save</button>
+                        </div>
+                      </form>
                     </div>
-                    <form id="inputBox" action="submit" method="post">
-                      <div>
-                        <!-- <label for="title">일정</label> -->
-                        <input id="title" type="text" name="title" placeholder="일정" />
-                      </div>
-                      <div>
-                        <!-- <label for="time">시간</label> -->
-                        <input id="time" type="time" name="time" placeholder="시간" />
-                      </div>
-                      <div>
-                        <!-- <label for="place">장소</label> -->
-                        <input id="place" type="text" name="place" placeholder="장소" />
-                      </div>
-                      <div>
-                        <!-- <label for="memo">메모</label> -->
-                        <input id="memo" type="text" name="memo" placeholder="메모" />
-                      </div>
-                      <div>
-                        <button id="saveBtn" type="submit">Save</button>
-                      </div>
-                    </form>
                   </div>
                 </div>
-              </div>
-              <script src="script.js"></script>
-            </body>
-          </html>
+                <script src="script.js"></script>
+              </body>
+            </html>
           `;
 
           // * index 다시 생성
-          fs.writeFileSync(
+          fs.writeFile(
             path.join(__dirname, "public", "index.html"),
             mainIdx,
             (err) => {
-              err500(err);
+              if (err) err500(res);
               console.log("index 다시 생성 !");
 
               // * index 다시 읽기
               fs.readFile(
                 path.join(__dirname, "public", "index.html"),
                 (err, data) => {
-                  err500(err);
+                  if (err) err500(res);
                   res.writeHead(200, {
                     "Content-Type": mime.html,
                   });
@@ -201,7 +186,7 @@ const server = http.createServer((req, res) => {
       err404(res);
     }
   } else {
-    err404();
+    err404(res);
   }
 });
 
