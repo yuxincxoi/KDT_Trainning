7f24690a1499e9024565eb803596ed0a7141be7d
yuxincxoi <yjyj0774@gmail.com>
2024-06-10 11:43:34 +0900

    error message 모듈 추가 및 경로 수정

--- test/test5/modules/getMethod.js
+++ test/test5/modules/getMethod.js
@@ -1,14 +1,15 @@
 const path = require("path");
 const mimeType = require("./mimeType");
 const readFile = require("./readFile");
+const errMsg = require("./errMsg");
 
 const getMethod = (req, res) => {
   if (req.url === "/") {
-    readFile(path.join(__dirname, "public", "index.html"), mimeType.html, res);
+    readFile(path.join("./", "public", "index.html"), mimeType.html, res);
   } else if (req.url === "/style.css") {
-    readFile(path.join(__dirname, "public", "style.css"), mimeType.css, res);
+    readFile(path.join("./", "public", "style.css"), mimeType.css, res);
   } else if (req.url === "/script.js") {
-    readFile(path.join(__dirname, "public", "script.js"), mimeType.js, res);
+    readFile(path.join("./", "public", "script.js"), mimeType.js, res);
   } else {
     res.writeHead(404, { "Content-Type": mimeType.text });
     res.end(errMsg[404]);
