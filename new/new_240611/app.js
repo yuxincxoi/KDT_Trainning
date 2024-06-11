e0d276c888074c5a4c055324dfea95362eddf44a
yuxincxoi <yjyj0774@gmail.com>
2024-06-11 10:51:47 +0900

    변수 할당

--- new/new_240611/app.js
+++ new/new_240611/app.js
@@ -1,12 +1,18 @@
-const a = () => {
+// 캡슐화
+const a = (a, b, c) => {
   return {
-    name: "",
-    age: 0,
-    job: "",
+    name: a,
+    age: b,
+    job: c,
   };
 };
 
 // console.log(a());
 
-const b = a();
-console.log(b);
+// 정적 할당
+const b = a("최유진", 20, "student");
+console.log("첫번째 : ", b);
+
+// 동적 할당
+b.address = "대전";
+console.log("두번째 : ", b);
