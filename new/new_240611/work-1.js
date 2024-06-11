2294c691837069e752bd6451fe929e51fc6de272
yuxincxoi <yjyj0774@gmail.com>
2024-06-11 11:41:47 +0900

    컴포넌트 작업

--- new/new_240611/work-1.js
+++ new/new_240611/work-1.js
@@ -1,14 +1,23 @@
-function formatHTML() {
+function formatHTML(language) {
   let htmlData = {
     title: "최유진은",
+    subTitle: "yujinchoi is",
     h1: "약을 먹어서",
     p: "졸린가?",
+  };
+
+  let decision = () => {
+    if (language === "ko") {
+      return htmlData.title;
+    } else if (language === "en") {
+      return htmlData.subTitle;
+    }
   };
 
   let html = `
   <html>
     <head>
-      <title>${htmlData.title}</title>
+      <title>${decision()}</title>
     </head>
     <body>
       <h1>${htmlData.h1}</h1>
@@ -20,4 +29,5 @@ function formatHTML() {
   return html;
 }
 
-console.log(formatHTML());
+console.log(formatHTML("ko"));
+console.log(formatHTML("en"));
