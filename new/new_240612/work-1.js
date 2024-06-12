8a2e7af66fc4226b4bd1825af4c251d7cfa982d2
yuxincxoi <yjyj0774@gmail.com>
2024-06-12 11:43:12 +0900

    생성자 함수 return값 string으로 받아오기

--- new/new_240612/work-1.js
+++ new/new_240612/work-1.js
@@ -1,15 +1,54 @@
-const basicData = () => {
-  return {
-    name: "최유진",
-    age: 20,
-    job: "개발자",
-  };
-};
+class BasicData {
+  constructor(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+  }
+
+  set Name(name) {
+    if (typeof name === "string") {
+      this._name = name;
+    }
+  }
+  get Name() {
+    return this._name;
+  }
+
+  set Age(age) {
+    if (typeof age === "number") {
+      this._age = age;
+    }
+  }
+  get Age() {
+    return this._age;
+  }
+
+  set Job(job) {
+    if (typeof job === "string") {
+      this._job = job;
+    }
+  }
+  get Job() {
+    return this.job;
+  }
+}
 
 const component = () => {
-  let obj = basicData();
+  let obj = new BasicData("최유진", 20, "개발자");
+
+  if (obj instanceof BasicData) {
+    return `
+    이름 : ${obj.name},
+    나이 : ${obj.age},
+    직업 : ${obj.job}
+    `;
+  } else {
+    throw new Error();
+  }
 
   const decision = () => {};
 };
+
+console.log(component());
 
 return `<html></html>`;
