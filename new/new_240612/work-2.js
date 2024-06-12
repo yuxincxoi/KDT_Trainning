2984bec50fbe200415fda0e1b73381012fd97886
yuxincxoi <yjyj0774@gmail.com>
2024-06-12 11:48:50 +0900

    obj 정적 할당

--- new/new_240612/work-2.js
+++ new/new_240612/work-2.js
@@ -1,7 +1,13 @@
 const component = () => {
-  let obj = {};
+  let obj = {
+    name: "최유진",
+    age: 20,
+    job: "개발자",
+  };
 
   let innerFunc = () => {};
-};
 
-return `<html></html>`;
+  return `
+  <h1>${obj.name}</h1>
+  `;
+};
