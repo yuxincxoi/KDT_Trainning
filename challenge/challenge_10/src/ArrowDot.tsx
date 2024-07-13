66dcec0d337abb3bbbf1d0056cf6a01884eff917
yuxincxoi <yjyj0774@gmail.com>
2024-07-13 17:07:14 +0900

    arrow dot style 추가

--- challenge/challenge_10/src/ArrowDot.tsx
+++ challenge/challenge_10/src/ArrowDot.tsx
@@ -1,5 +1,13 @@
+import { CSSProperties } from "react";
+
 const ArrowDot: React.FC = () => {
-  return <div></div>;
+  let styles: CSSProperties = {
+    width: "23px",
+    height: "40px",
+    backgroundColor: "#ffb14f",
+    transform: "rotate(90deg)",
+  };
+  return <div style={styles}></div>;
 };
 
 export default ArrowDot;
