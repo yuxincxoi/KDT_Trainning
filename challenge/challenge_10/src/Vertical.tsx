93606a4088d5346380cd0078308549115f5975bb
yuxincxoi <yjyj0774@gmail.com>
2024-07-13 17:06:57 +0900

    vertical style 추가

--- challenge/challenge_10/src/Vertical.tsx
+++ challenge/challenge_10/src/Vertical.tsx
@@ -1,9 +1,19 @@
 import VerticalLine from "./VerticalLine";
 import VerticalDot from "./VerticalDot";
+import { CSSProperties } from "react";
 
 const Vertical: React.FC = () => {
+  let styles: CSSProperties = {
+    top: "650px",
+    left: "1280px",
+    position: "absolute",
+    width: "10px",
+    height: "10px",
+    background: "#977240",
+    borderRadius: "5px",
+  };
   return (
-    <div>
+    <div style={styles}>
       <VerticalLine />
       <VerticalDot />
     </div>
