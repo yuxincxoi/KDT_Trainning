b7383abd3112d3b48dd22d3fc5a4e4f7e0e9900e
yuxincxoi <yjyj0774@gmail.com>
2024-07-13 15:28:01 +0900

    card 컴포넌트 생성

---
+++ challenge/challenge_10/src/card.tsx
@@ -0,0 +1,15 @@
+import CardTitle from "./CardTitle";
+import CardLine from "./CardLine";
+import CardText from "./CardText";
+
+const Card: React.FC = () => {
+  return (
+    <div>
+      <div>
+        <CardTitle />
+        <CardLine />
+      </div>
+      <CardText />
+    </div>
+  );
+};
