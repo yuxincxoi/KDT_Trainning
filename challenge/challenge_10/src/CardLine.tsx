b9f1f41b721c12d9d290c63d5a35ef1f4e37f0f0
yuxincxoi <yjyj0774@gmail.com>
2024-07-13 17:01:41 +0900

    card line style 추가

--- challenge/challenge_10/src/CardLine.tsx
+++ challenge/challenge_10/src/CardLine.tsx
@@ -1,5 +1,16 @@
+import { CSSProperties } from "react";
+
 const CardLine: React.FC = () => {
-  return <div></div>;
+  let styles: CSSProperties = {
+    top: "72px",
+    left: "475px",
+    width: "290px",
+    height: "1px",
+    position: "absolute",
+    background:
+      "linear-gradient(90deg, rgba(210, 143, 67, 0.5) 0%, rgba(80, 47, 10, 0.5) 100%)",
+  };
+  return <div style={styles}></div>;
 };
 
 export default CardLine;
