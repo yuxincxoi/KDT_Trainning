b5539ddc9a6626fcf945f2a6ef9a9c439165df4c
yuxincxoi <yjyj0774@gmail.com>
2024-07-13 17:06:40 +0900

    vertical line style 추가

--- challenge/challenge_10/src/VerticalLine.tsx
+++ challenge/challenge_10/src/VerticalLine.tsx
@@ -1,5 +1,17 @@
+import { CSSProperties } from "react";
+
 const VerticalLine: React.FC = () => {
-  return <div></div>;
+  let styles: CSSProperties = {
+    top: "410px",
+    left: "1120px",
+    width: "330px",
+    height: "2px",
+    position: "absolute",
+    background:
+      "linear-gradient(90deg, rgba(210,143, 67, 0.5) 0%, rgba(80, 47, 10, 0.5) 100%)",
+    transform: "rotate(90deg)",
+  };
+  return <div style={styles}></div>;
 };
 
 export default VerticalLine;
