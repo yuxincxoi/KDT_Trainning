2fa33cc9f9aeafd4dd5fcc65c53c10f23d2f0db3
yuxincxoi <yjyj0774@gmail.com>
2024-07-13 17:02:34 +0900

    card title style 추가

--- challenge/challenge_10/src/CardTitle.tsx
+++ challenge/challenge_10/src/CardTitle.tsx
@@ -1,5 +1,19 @@
+import { CSSProperties } from "react";
+
 const CardTitle: React.FC = () => {
-  return <h1>INDENTATION</h1>;
+  let styles: CSSProperties = {
+    top: "20px",
+    left: "110px",
+    position: "absolute",
+    fontFamily: "Inter",
+    fontStyle: "normal",
+    fontWeight: "400",
+    fontSize: "60px",
+    lineHeight: "77px",
+    textAlign: "justify",
+    color: "#23201a",
+  };
+  return <h1 style={styles}>INDENTATION</h1>;
 };
 
 export default CardTitle;
